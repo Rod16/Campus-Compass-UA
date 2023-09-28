ea3a7400bd788cd510056a4c227c3715332b33c9
Rod16 <litvineenko.rodion@gmail.com>
2023-09-28 22:42:45 +0300

    Unused imports

--- src/app/pages/university-authority/services/university-authority.service.ts
+++ src/app/pages/university-authority/services/university-authority.service.ts
@@ -1,4 +1,4 @@
-import {Injectable, OnInit} from "@angular/core";
+import {Injectable} from "@angular/core";
 import {AngularFirestore} from "@angular/fire/compat/firestore";
 import {IUserInfo} from "../../../shared/interfaces/user-info";
 import {switchMap} from "rxjs";
