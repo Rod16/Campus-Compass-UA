0ea8c1b106c334ab2dbd80b640b6499490d8da22
Rod16 <litvineenko.rodion@gmail.com>
2023-11-05 23:08:30 +0200

    Wrapper functionality, UI

--- src/app/shared/resolvers/parent-data.resolver.ts
+++ src/app/shared/resolvers/parent-data.resolver.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot } from '@angular/router';
-import {SharedService} from "../services/shared.service";
 import {IUserInfo} from "../interfaces/user-info";
-import {Observable} from "rxjs";
 
 @Injectable({ providedIn: 'root' })
 export class ParentDataResolver {
